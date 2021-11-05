e08797bbf36bf6339a382577ae238c216defd97d
DoctorPasteur <jorgepastor66@outlook.com>
2021-11-04 22:52:50 -0500

    updated corresponding test in carts_controller_test

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -39,9 +39,12 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy cart" do
+    post line_items_url, params: {product_id: products(:ruby).id}
+    @cart = Cart.find(session[:cart_id])
+    
     assert_difference('Cart.count', -1) do
       delete cart_url(@cart)
-    end
+  end
 
     assert_redirected_to carts_url
   end
